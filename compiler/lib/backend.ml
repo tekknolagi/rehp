1e7b6b65682a2c7da8d4fa6ad10887b473209215
Max Bernstein <emacs@fb.com>
2019-12-04 22:54:07 -0800

    Change backend selection implementation to use alist
    This is slightly less clunky compared to a tree of ifs

--- compiler/lib/backend.ml
+++ compiler/lib/backend.ml
@@ -2,15 +2,16 @@ type t =
   | Php
   | Js
 
-let to_string t = match t with
-| Php -> "php"
-| Js -> "js"
+let backend_extensions = ["php"; "js"]
+
+let backends = [Php; Js]
+
+let to_string t =
+  List.assoc t (List.combine backends backend_extensions)
 
 let from_string s =
-  if s = "php" then
-    Php
-  else
-    if s = "js" then Js
-    else raise (Invalid_argument (s ^ " is not a valid backend"))
+  match List.assoc_opt s (List.combine backend_extensions backends) with
+  | Some backend -> backend
+  | None -> raise (Invalid_argument (s ^ " is not a valid backend"))
 
 let extension = to_string
